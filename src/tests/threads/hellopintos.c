c51c7802560a80f9e216d0048a05f54ce7e91c2d
tejus agarwal <tejusagarwal13@gmail.com>
2018-10-31 23:42:31 +0530

    FIRST COMMIT

---
+++ src/tests/threads/hellopintos.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "tests/threads/tests.h"
+#include "threads/malloc.h"
+#include "threads/synch.h"
+#include "threads/thread.h"
+#include "devices/timer.h"
+
+void hello_pintos()
+{
+  printf("Hello pintos");
+}
+
---
+++ src/tests/threads/hellopintos.ck
@@ -0,0 +1,10 @@
+# -*- perl -*-
+use strict;
+use warnings;
+use tests::tests;
+check_expected ([<<'EOF']);
+(hellopintos) begin
+(hellopintos) PASS
+(hellopintos) end
+EOF
+pass;
